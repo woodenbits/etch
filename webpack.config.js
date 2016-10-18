accd86490bae322d47ff48a795e26f6f7c2bebb8
Hargobind S. Khalsa <khalsah@gmail.com>
2016-10-17 23:26:10 -0600

    Setup hot reloading

--- webpack.config.js
+++ webpack.config.js
@@ -15,31 +15,28 @@ if (optimize) {
 
 module.exports = {
   context: __dirname,
-  devtool: debug ? 'eval-source-map' : null,
+  devtool: debug ? 'inline-source-map' : null,
   target: 'electron',
   entry: {
-    main: './lib/main',
-    renderer: './lib/renderer',
+    main: ['./lib/support/webpack', './lib/main'],
+    renderer: ['./lib/support/webpack', './lib/renderer'],
   },
   output: {
     filename: '[name].bundle.js',
     path: './app/assets',
     pathinfo: debug,
-    publicPath: 'assets://',
-  },
-  node: {
-    __dirname: false,
-    __filename: false,
   },
   resolve: {
     extensions: ['', '.js', '.jsx', '.json'],
+  },
+  node: {
+    __dirname: false,
   },
   module: {
     loaders: [
       { test: /\.json$/, loader: 'json' },
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel' },
       { test: /\.(jpe?g|png|gif|svg)$/, loaders: ['url', 'img?minimize'] },
-      { test: /\.html$/, loader: 'file' },
     ],
   },
   plugins,
