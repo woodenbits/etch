2146086565a7a3481bb8b8cce427e1d7da46336a
Hargobind S. Khalsa <khalsah@gmail.com>
2016-10-17 23:36:05 -0600

    Setup fallback page reload for hot reloading

--- lib/renderer.jsx
+++ lib/renderer.jsx
@@ -9,3 +9,12 @@ const styles = StyleSheet.create({
 });
 
 render(<h1 className={css(styles.message)}>Hello World!</h1>, document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept();
+  module.hot.dispose(() => {
+    // eslint-disable-next-line no-console
+    console.log('[HMR] Reloading');
+    window.location.reload();
+  });
+}
