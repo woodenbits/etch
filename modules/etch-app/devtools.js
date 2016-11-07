ad80aabf13e556e53787ddb2fe41cd7e19c7e5ec
Hargobind S. Khalsa <khalsah@gmail.com>
2016-11-06 22:08:39 -0700

    Add --force-dowload-devtools for updating devtools

--- modules/etch-app/devtools.js
+++ modules/etch-app/devtools.js
@@ -3,10 +3,12 @@
 import installExtension, { REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS, REACT_PERF } from 'electron-devtools-installer';
 
 export default async function () {
+  const force = process.argv.indexOf('--force-download-devtools') >= 0;
+
   await Promise.all([
-    installExtension(REACT_DEVELOPER_TOOLS),
-    installExtension(REDUX_DEVTOOLS),
-    installExtension(REACT_PERF),
+    installExtension(REACT_DEVELOPER_TOOLS, force),
+    installExtension(REDUX_DEVTOOLS, force),
+    installExtension(REACT_PERF, force),
   ]);
 
   // eslint-disable-next-line no-console
